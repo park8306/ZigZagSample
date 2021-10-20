0ec892bba3109eba2ac19fc21e005f0c24dd645d
park8306 <kosaf0804@gmail.com>
2021-10-20 21:26:35 +0900

    점수 UI 추가

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
@@ -12,14 +14,42 @@ public class Ball : MonoBehaviour
     Direction direction = Direction.Right;
     // Update is called once per frame
     public float speed = 5f;
+    private Quaternion originalRote;
+    public Text pointText;
+    int point = 0;
+    public float gameOverHeight = 1.3f;
+    private void Start()
+    {
+
+        originalRote = transform.rotation;
+    }
+
     void Update()
     {
+        if(transform.position.y < gameOverHeight)
+        {
+            Debug.Log("게임오버");
+            return;
+        }
+
         if(Input.anyKeyDown)
         {
             direction = (direction == Direction.Right) ? Direction.Left : Direction.Right;
+            var velocity = GetComponent<Rigidbody>().velocity;
+            velocity.x = 0;
+            velocity.z = 0;
+            GetComponent<Rigidbody>().velocity = velocity;
+            pointTextAdd(1);
         }
+        transform.rotation = originalRote;
 
         BallMove();
+    }
+
+    private void pointTextAdd(int point)
+    {
+        this.point += point;
+        pointText.text = this.point.ToString();
     }
 
     private void BallMove()

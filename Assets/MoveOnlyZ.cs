0f6fc2f90bdfddba3a9dd02d05f410cb3b03711d
park8306 <kosaf0804@gmail.com>
2021-10-20 20:46:05 +0900

    카메라 구조 변경에 따른 스크립트 작업

--- Assets/MoveOnlyZ.cs
+++ Assets/MoveOnlyZ.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class MoveOnlyZ : MonoBehaviour
 {
+    public Transform followTr;
+    public Vector3 offset;
+
     public float x;
     public float y;
-    Quaternion rotation;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        offset = transform.position - followTr.position;
         x = transform.position.x;
         y = transform.position.y;
-        rotation = transform.rotation;
     }
 
-    // Update is called once per frame
-    void LateUpdate()   // 나중에 업데이트 모든 업데이트가 다 실행된 후에 실행 됨.
+    private void Update()
     {
-        var pos = transform.position;
+        var pos =  offset + followTr.position;
         pos.x = x;
         pos.y = y;
         transform.position = pos;
-        transform.rotation = rotation;
     }
+    //public float x;
+    //public float y;
+    //Quaternion rotation;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    x = transform.position.x;
+    //    y = transform.position.y;
+    //    rotation = transform.rotation;
+    //}
+
+    //// Update is called once per frame
+    //void LateUpdate()   // 나중에 업데이트 모든 업데이트가 다 실행된 후에 실행 됨.
+    //{
+    //    var pos = transform.position;
+    //    pos.x = x;
+    //    pos.y = y;
+    //    transform.position = pos;
+    //    transform.rotation = rotation;
+    //}
 }
